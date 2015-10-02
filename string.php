e4a190219874dbc7809d23e414fce47b7f7e639b
MrAbdulbasit <mr.abdulbasit1@gmail.com>
2015-10-03 03:21:35 +0500

    Hello World PHP

---
+++ string.php
@@ -0,0 +1,22 @@
+<?php
+echo "Hello world 1<br>";
+echo "Hello world 2<br>";
+$my_variable = "Hello world 3";
+echo $my_variable;
+echo "<br>";
+echo $my_variable . "Again <br>";
+
+$var_1 = 10;
+$var_2 = 20;
+$var_3 = $var_1 + $var_2;
+
+echo "total of 2 variable is  = " . $var_3;
+
+?>
+<br/>
+
+<?php
+echo "$my_variable Again. <br>";
+echo "{$my_variable} Again. <br>";
+echo '$my_variable Again. <br>';
+?>
---
+++ variable.php
@@ -0,0 +1,13 @@
+<?php
+$var_1 = 100;
+echo $var_1 . "<br/>" ;
+
+$var_1 = 1000;
+echo $var_1 ." this is num 1000 <br>"  ;
+
+
+$a = 10;
+$b = 20;
+$c = $a + $b;
+print $c;
+?>
